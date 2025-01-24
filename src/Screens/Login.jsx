c668d44179a2ebe7ae55d00eed8fd5ec5b84217a
felipejimcam25 <andreyfelipejimenezcampos@gmail.com>
2025-01-23 18:00:38 -0600

    styles corrections

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -3,9 +3,14 @@ import { loginEndPoint } from '../spotify'
 import Logo from '../Resources/Logo/Logo.png';
 export default function Login() {
     return (
-        <div className='container'>
+        <div className='container login-content'>
+            <div className="logo-container">
             <img src={Logo} alt="Logo BeRhythm" className='login_header' />
-            <a href={loginEndPoint} className='login_btn'>Log In</a>
+            </div>
+            <div className="login-btn-container">
+                <a href={loginEndPoint} className='login_btn'>Log In</a>
+            </div>
+            
         </div>
     )
 }
